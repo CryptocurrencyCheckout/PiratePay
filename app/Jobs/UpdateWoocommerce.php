9ca37b1fb76c67b7427a8a176bb5cb1f2774f55d
Ratstang <support@cryptocurrencycheckout.com>
2020-10-08 16:03:13 -0600

    Add Crypto to UpdateWoocommerce

--- app/Jobs/UpdateWoocommerce.php
+++ app/Jobs/UpdateWoocommerce.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Http;
 use App\Transaction;
 use Carbon\Carbon;

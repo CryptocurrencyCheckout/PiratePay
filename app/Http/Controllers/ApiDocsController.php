40f308e13cd4365de473d358c39df6679ecc382a
CryptocurrencyCheckout <51866643+CryptocurrencyCheckout@users.noreply.github.com>
2020-12-05 10:54:28 -0700

    Require Login to view API Documentation

--- app/Http/Controllers/ApiDocsController.php
+++ app/Http/Controllers/ApiDocsController.php
@@ -8,6 +8,18 @@ class ApiDocsController extends Controller
 {
 
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('throttle:30');
+    }
+    
+
+    /**
      * Show the API Documentation Home Page.
      *
      * @return \Illuminate\Contracts\Support\Renderable
